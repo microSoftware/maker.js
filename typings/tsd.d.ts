3a17e24b98f1dd043292fcf9d85d8283eecbdce7
Dan Marshall <danmar@microsoft.com>
2016-06-09 08:29:03 -0700

    added bezier-js dependency

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="OpenJsCad/csg.d.ts" />
 /// <reference path="pdfkit/pdfkit.d.ts" />
 /// <reference path="text-encoding/text-encoding.d.ts" />
+/// <reference path="bezier-js/bezier-js.d.ts" />
