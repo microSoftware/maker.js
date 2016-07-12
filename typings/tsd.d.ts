3320cb4a5f1b37452ad0f9fbad9084012222187c
Dan Marshall <danmar@microsoft.com>
2016-07-11 23:43:59 -0700

    tsd updates

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,3 +4,5 @@
 /// <reference path="pdfkit/pdfkit.d.ts" />
 /// <reference path="text-encoding/text-encoding.d.ts" />
 /// <reference path="bezier-js/bezier-js.d.ts" />
+/// <reference path="codemirror/codemirror.d.ts" />
+/// <reference path="marked/marked.d.ts" />
